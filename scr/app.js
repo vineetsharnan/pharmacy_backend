4673faf57192d3e65e210f5aa4e79f0624dfa3ee
vineetsharnan <vineetsharnan2000@gmail.com>
2026-06-27 17:37:37 +0530

    folder structure created

---
+++ scr/app.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const app=express();
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to Pharmacy Backend 🚀 ");
+});
+
+module.exports=app;
---
+++ server.js
@@ -0,0 +1,8 @@
+const app=require("./scr/app");
+require("dotenv").config();
+
+const PORT=process.env.PORT;
+
+app.listen(PORT,()=>{
+     console.log(`${process.env.APP_NAME} running on port ${PORT}`);
+});
