492fc0dbeee8f5df3586e9d30d717bcb3154df04
vineetsharnan <vineetsharnan2000@gmail.com>
2026-06-28 11:39:46 +0530

    Rest methods created

--- scr/app.js
+++ scr/app.js
@@ -2,11 +2,91 @@ const express=require('express');
 const app=express();
 
 
-
+app.use(express.json());
 
 
 app.get('/',(req,res)=>{
-    res.send("Welcome to Pharmacy Backend 🚀 ");
+    res.send("Welcome to Pharmacy Backend  ");
 });
 
+//--------------------------------------------Get------------------------------------
+
+app.get('/products',(req,res)=>{
+
+    const category=req.query.category;
+    const products=[
+        {
+            id:1,
+            name:"Crosin 650",
+            price:35,
+            category:category
+        },{
+            id:2,
+            name:"Dolo 650",
+            price:40,
+            category:category        }
+    ];
+    res.json(products);
+});
+
+app.get("/products/:id",(req,res)=>{
+
+    const id=req.params.id;
+
+    res.status(200).json({
+        message:"Product found",
+        productsId:id
+    });
+});
+
+//--------------------------------------------Get End------------------------------------
+
+
+
+//--------------------------------------------POST------------------------------------
+
+app.post("/products", (req,res)=>{
+
+    const product=req.body;
+
+    res.status(201).json({
+        message:"Product created successfully",
+        data:product
+    });
+
+});
+
+//--------------------------------------------POST End------------------------------------
+
+//--------------------------------------------PUT------------------------------------
+
+app.put("/products/:id",(req,res)=>{
+
+    res.status(200).json({
+        message:"Product updated sucessfully",
+         id:req.params.id,
+         data:req.body 
+    });
+});
+//--------------------------------------------PUT End------------------------------------
+
+//--------------------------------------------Delete------------------------------------
+
+app.delete("/products/:id", (req,res)=>{
+
+
+    res.status(200).json({
+        message:"Deleted Sucsessfully",
+        id:req.params.id
+    });
+
+
+});
+
+
+//--------------------------------------------Delete End------------------------------------
+
+
+
 module.exports=app;
+
--- server.js
+++ server.js
@@ -6,3 +6,4 @@ const PORT=process.env.PORT;
 app.listen(PORT,()=>{
      console.log(`${process.env.APP_NAME} running on port ${PORT}`);
 });
+
